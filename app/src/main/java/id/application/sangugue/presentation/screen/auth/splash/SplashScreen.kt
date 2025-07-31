d0909d5852d17d52da2df71adb9600d6b314ef18
rifqi.siliwangi <rifqipadisiliwangi@gmail.com>
2025-07-31 14:14:06 +0700

    ref add logo to splash screen

--- app/src/main/java/id/application/sangugue/presentation/screen/auth/splash/SplashScreen.kt
+++ app/src/main/java/id/application/sangugue/presentation/screen/auth/splash/SplashScreen.kt
@@ -1,8 +1,13 @@
 package id.application.sangugue.presentation.screen.auth.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,10 +15,13 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import id.application.sangugue.R
 import id.application.sangugue.presentation.navigation.Screen
 import id.application.sangugue.ui.theme.PLNBlueDark
 import id.application.sangugue.ui.theme.White
@@ -36,6 +44,24 @@ fun SplashScreen(navController: NavHostController) {
             .background(PLNBlueDark),
         contentAlignment = Alignment.Center
     ) {
-        Text("Sangu", color = Color.White, fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = "Logo Sangu",
+                modifier = Modifier
+                    .size(96.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "SanguGue",
+                color = Color.White,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
